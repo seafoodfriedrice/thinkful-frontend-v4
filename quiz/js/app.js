6dcf2a7de9ca80fd2b9538f65fde92cce2cd6cd5
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-10-08 00:54:38 +0400

    quiz: add quiz content inside dict and setup answe options

--- quiz/js/app.js
+++ quiz/js/app.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
+  quiz = {
+    "1": {
+      "question": "The main character of the movie, Po, is voiced by which actor?",
+      "answers": ["Channing Tatum", "Jack Black", "Tom Cruise", "Kyle Gass"],
+      "correct": "Jack Black"
+    },
+    "2": {
+      "question": "What is the name of Po's father?",
+      "answers": ["Mr. Ping", "Mr. Chen", "Mr. Panda", "Mr. Chang"],
+      "correct": "Mr. Ping"
+    },
+    "3": {
+      "question": "What is the secret ingredient in the Secret Ingredient Soup?",
+      "answers": ["Tofu", "Green Onions", "Special Soy Sauce", "Nothing"],
+      "correct": "Nothing"
+    },
+    "4": {
+      "question": "Who is not part of the Furious Five?",
+      "answers": ["Tigress", "Shifu", "Mantis", "Viper"],
+      "correct": "Shifu"
+    },
+    "5": {
+      "question": "What type of animal is Master Shifu?",
+      "answers": ["Flying Squirrel", "Red Panda", "Raccoon", "Polar Bear"],
+      "correct": "Red Panda"
+    }
+  }
+
+  var q = "<strong>Question</strong>: ";
+  $(".question-content").html(q + quiz[2]["question"]);
+  var checkIconHtml = '<i class="fa fa-check"></i>';
+  $("ul").children().each(function(i, li) {
+    $(this).html(quiz[2]["answers"][i] + checkIconHtml);
+  });
+
+
   $(".answers-content li").click(function() {
     $(".answers-content").addClass("active");
     $("li").removeClass("selected");
     $(this).addClass("selected");
+    var answer = $(this).text();
   });
 });
